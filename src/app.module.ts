802f4a7c34c5bacccc4919a2fb7e3127abe4602c
Juan  Marquina <juanantoniomarquina@gmail.com>
2025-01-12 18:05:54 -0500

    feat: add model user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
+
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+
     AuthModule,
-    MongooseModule.forRoot(''),
+    MongooseModule.forRoot( process.env.MONGO_URI ),
   ],
   controllers: [AppController],
   providers: [AppService],
